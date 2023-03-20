51638718e2b27a8ffb0852820adaf5d3c0d676b1
Sacha STAFYNIAK <sacha@digisquad.io>
2023-03-20 13:59:27 +0100

    feat: upgrade to astro 2 + add unplugin-fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,23 @@
 import { defineConfig } from 'astro/config';
+import Unfonts from 'unplugin-fonts/astro'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    Unfonts({
+      google: {
+        families: [
+          'Material+Icons',
+          {
+            name: 'Roboto',
+            styles: 'wght@300;400;500;700',
+          },
+          {
+            name: 'Nunito',
+            styles: 'wght@400;600;700;800'
+          },
+        ],
+      },
+    })
+  ]
+});
