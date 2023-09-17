c00295507774d988e2f7d332c382dc9dc7bca674
Sacha STAFYNIAK <sacha@digisquad.io>
2023-09-17 16:23:06 +0200

    feat: use variable fonts from fontsources

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,4 @@
 import { defineConfig } from 'astro/config';
-import Unfonts from 'unplugin-fonts/astro'
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [
-    Unfonts({
-      google: {
-        families: [
-          'Material+Icons',
-          {
-            name: 'Roboto',
-            styles: 'wght@300;400;500;700',
-          },
-          {
-            name: 'Nunito',
-            styles: 'wght@400;600;700;800'
-          },
-        ],
-      },
-    })
-  ]
-});
+export default defineConfig({});
